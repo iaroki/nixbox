e363d0dc712db4c6564e9ae7dc310a3e04dbf38c
iaroki <iaroki@protonmail.com>
2021-10-24 13:17:56 +0300

    add customization

--- scripts/custom-configuration.nix
+++ scripts/custom-configuration.nix
@@ -1,6 +1,28 @@
 { config, pkgs, ... }:
 
 {
-# Place here any custom configuration specific to your organisation (locale, ...)
-# if you want it to be part of the packer base image to be used with vagrant.
+    time.timeZone = "Europe/Kiev";
+
+    environment.systemPackages = with pkgs; [
+    wget vim tmux git htop tree ripgrep unzip
+    terraform_0_14 terraform-docs tflint terragrunt
+    ansible ansible-lint awscli2 kubectl helm
+    go python38 python38Packages.pip skopeo buildah
+
+  ];
+
+  environment.homeBinInPath = true;
+  programs.vim.defaultEditor = true;
+
+  users.users.msytnyk = {
+    isNormalUser = true;
+    createHome = true;
+    home = "/home/msytnyk";
+    extraGroups = [ "wheel" "docker" ];
+  };
+
+  virtualisation.docker.enable = true;
+  virtualisation.podman.enable = true;
+
+  nixpkgs.config.allowUnfree = true;
 }
