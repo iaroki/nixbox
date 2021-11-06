a8d79addbc08edd7f7468d23dcd9b4d9fe7e4840
iaroki <iaroki@protonmail.com>
2021-11-06 22:21:49 +0200

    add neovim

--- scripts/home-manager.nix
+++ scripts/home-manager.nix
@@ -1,6 +1,6 @@
 { config, pkgs, ... }:
 let
-    home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+    home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-21.05.tar.gz";
 in
 {
     imports = [
@@ -8,11 +8,11 @@ in
     ];
 
     home-manager.users.msytnyk = {
-        home.enableNixpkgsReleaseCheck = false;
-
         home.packages = [
             pkgs.exa
             pkgs.bat
+            pkgs.fzf
+            pkgs.ctags
             pkgs.ripgrep
             pkgs.jq
             pkgs.yq
@@ -27,6 +27,7 @@ in
             pkgs.kubectl
             pkgs.helm
             pkgs.go
+            pkgs.gopls
             pkgs.python38
             pkgs.python38Packages.pip
             pkgs.skopeo
@@ -37,9 +38,83 @@ in
             LANG = "en_US.UTF-8";
             LC_CTYPE = "en_US.UTF-8";
             LC_ALL = "en_US.UTF-8";
-            EDITOR = "vim";
+            EDITOR = "nvim";
             PAGER = "less -FirSwX";
             MANPAGER = "less -FirSwX";
+        };
+
+        programs.neovim = {
+	    enable = true;
+	    plugins = with pkgs.vimPlugins; [
+                gruvbox-community
+                lightline-vim
+                vim-terraform
+                vim-terraform-completion
+                vim-hcl
+                vim-nix
+                vim-go
+                deoplete-nvim
+                fzf-vim
+                tagbar
+                nerdtree
+                nerdtree-git-plugin
+                vim-surround
+                vim-commentary
+                vim-fugitive
+                vim-gitgutter
+                rainbow
+                vim-better-whitespace
+                indentLine
+                vim-devicons
+            ];
+            viAlias = true;
+            vimAlias = true;
+            vimdiffAlias = true;
+            extraConfig = ''
+                map <C-n> :NERDTreeToggle<CR>
+                nmap <C-P> :FZF<CR>
+                inoremap kj <esc>
+                cnoremap kj <C-C>
+
+                colorscheme gruvbox
+                set background=dark
+                set number
+                syntax on
+                set encoding=utf-8
+                set laststatus=2
+                set noshowmode
+                set incsearch
+                set hlsearch
+                set smarttab
+                set expandtab
+                set tabstop=2
+                set shiftwidth=2
+                set softtabstop=2
+                set smartindent
+                set ignorecase
+                set smartcase
+                set scrolloff=8
+                set cursorline
+                set splitbelow
+                set splitright
+
+                let g:indentLine_color_dark = 1
+                let g:indentLine_char = '┊'
+                let g:deoplete#enable_at_startup = 1
+                let g:better_whitespace_enabled=1
+                let g:strip_whitespace_on_save=1
+                let g:rainbow_active = 1
+                let g:lightline = {
+                    \ 'colorscheme': 'gruvbox',
+                    \ 'active': {
+                    \   'left': [ [ 'mode', 'paste' ],
+                    \             [ 'gitbranch', 'readonly', 'filename', 'modified' ] ]
+                    \ },
+                    \ 'component_function': {
+                    \   'gitbranch': 'gitbranch#name'
+                    \ },
+                    \ }
+            '';
         };
 
         programs.bash = {
@@ -115,10 +190,10 @@ in
                 set-option -g status-right-style none
                 set-option -g status-right-length "80"
                 set-window-option -g window-status-separator ""
-                set-option -g status-left "#[bg=colour241,fg=colour248] #S #[bg=colour237,fg=colour241,nobold,noitalics,nounderscore]|"
-                set-option -g status-right "#[bg=colour237,fg=colour239 nobold, nounderscore, noitalics]|#[bg=colour239,fg=colour246] %Y-%m-%d | %H:%M #[bg=colour239,fg=colour248,nobold,noitalics,nounderscore]|#[bg=colour248,fg=colour237] #h "
-                set-window-option -g window-status-current-format "#[bg=colour214,fg=colour237,nobold,noitalics,nounderscore]|#[bg=colour214,fg=colour239] #I |#[bg=colour214,fg=colour239,bold] #W#{?window_zoomed_flag,*Z,} #[bg=colour237,fg=colour214,nobold,noitalics,nounderscore]|"
-                set-window-option -g window-status-format "#[bg=colour239,fg=colour237,noitalics]|#[bg=colour239,fg=colour223] #I |#[bg=colour239,fg=colour223] #W #[bg=colour237,fg=colour239,noitalics]|"
+                set-option -g status-left "#[bg=colour241,fg=colour248] #S #[bg=colour237,fg=colour241,nobold,noitalics,nounderscore]"
+                set-option -g status-right "#[bg=colour237,fg=colour239 nobold, nounderscore, noitalics]#[bg=colour239,fg=colour246] %Y-%m-%d | %H:%M #[bg=colour239,fg=colour248,nobold,noitalics,nounderscore]#[bg=colour248,fg=colour237] #h "
+                set-window-option -g window-status-current-format "#[bg=colour214,fg=colour237,nobold,noitalics,nounderscore]#[bg=colour214,fg=colour239] #I #[bg=colour214,fg=colour239,bold] #W#{?window_zoomed_flag,*Z,} #[bg=colour237,fg=colour214,nobold,noitalics,nounderscore]"
+                set-window-option -g window-status-format "#[bg=colour239,fg=colour237,noitalics]#[bg=colour239,fg=colour223] #I #[bg=colour239,fg=colour223] #W #[bg=colour237,fg=colour239,noitalics]"
             '';
         };
     };
